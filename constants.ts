de40e1fa606700af23624cea11b842b6ce1068b7
cuongnd258 <cuongnd258.work@gmail.com>
2025-05-27 21:39:36 +0700

    update

--- constants.ts
+++ constants.ts
@@ -1,5 +1,5 @@
 export const GEMINI_API_MODEL_TEXT = "gemini-2.5-flash-preview-04-17";
-export const API_BASE_URL = "http://localhost:3000"; // Backend API
+export const API_BASE_URL = "https://be-ota-cno.duckdns.org:3000"; // Backend API
 
 // This will be the base path for internal mock download routes.
 // e.g., #/app-downloads resulting in #/app-downloads/:projectId/:versionId
